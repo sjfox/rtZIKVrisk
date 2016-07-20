22a5ea2f0942785a26ecd5e50370fd53135b67d1
Spencer Fox <spencer.fox85@gmail.com>
2016-07-20 10:09:42 -0500

    figure 2 plotting functionality added

--- R/data.R
+++ R/data.R
@@ -54,4 +54,63 @@
 #' @source Zika epidemic simulations with R0=1, reporting rate=0.011, intro rate = 0.1, 15% symptomatic
 "growing_arrows"
 
+#' Simulated stochastic outbreak data
+#'
+#' Figure 2a data
+#'
+#' @format A data frame with 1108351 rows and 15 variables:
+#' \describe{
+#'   \item{risk_level}{risk level for simulation "High Risk" or "1.1"}
+#'   \item{disc_prob}{reporting rate of simulation}
+#'   \item{.id}{Simulation number used to separate individual outbreaks}
+#'   \item{time}{.id specific time of outbreak simulation}
+#'   \item{New_Exposed}{Newly exposed individuals}
+#'   \item{New_Infection}{Newly infectious individuals}
+#'   \item{Total_Infections}{Total number of infectious individuals}
+#'   \item{Cumulative_Infections}{Cumulative number of infectious individuals}
+#'   \item{New_Intro}{Newly imported (introduced) infectious cases}
+#'   \item{Total_Intro_Infections}{Total number of imported (introduced) infectious cases}
+#'   \item{Cumulative_Intro_Infections}{Cumulative number of imported (introduced) infectious cases}
+#'   \item{New_Detections}{Newly reported (detected) cases}
+#'   \item{Cum_Detections}{Cumulative number of detected cases for simulation}
+#'   \item{New_Intro_Detections}{Newly reported (detected) cases that were initially imported}
+#'   \item{Cum_Intro_Detections}{Cumulative reported (detected) cases that were initially imported}
+#'   ...
+#' }
+#' @source Zika epidemic simulations with R0=1.1 or high risk group, intro rate = 0.01 or high risk group
+"ex_outbreak_sims"
+
+#' Prevalence by reported cases data
+#'
+#' Figure 2b data
+#'
+#' @format A dataframe with 404 rows and 6 variables:
+#' \describe{
+#'   \item{risk_level}{risk level for simulation "High Risk" or "1.1"}
+#'   \item{disc_prob}{reporting rate of simulation}
+#'   \item{detected}{Number of reported autochthonous cases}
+#'   \item{median}{median autochthonous prevalence upon seeing x reported cases}
+#'   \item{min}{minimum of 50% CI for prevalence}
+#'   \item{max}{maximum of 50% CI for prevalence}
+#'   ...
+#' }
+#' @source Zika epidemic simulations with R0=1.1 or high risk group, intro rate = 0.01 or high risk group
+"prev_by_reported_data"
+
+#' Prevalence by reported cases data
+#'
+#' Figure 2b data
+#'
+#' @format A dataframe with 404 rows and 6 variables:
+#' \describe{
+#'   \item{risk_level}{risk level for simulation "High Risk" or "1.1"}
+#'   \item{disc_prob}{reporting rate of simulation}
+#'   \item{detected}{Number of reported autochthonous cases}
+#'   \item{value}{probability of an epidemic given you've detected x cases}
+#'   ...
+#' }
+#' @source Zika epidemic simulations with R0=1.1 or high risk group, intro rate = 0.01 or high risk group
+"epi_prob_by_reported_data"
+
+
 
--- R/figure_fxns.R
+++ R/figure_fxns.R
@@ -1,13 +1,12 @@
 
 #' Plot Fig 1 from Zika risk assessment
 #'
-#' @return A
+#' @return A ggplot object with cowplot theme that contains both figure panels
 #' @export
 #' @import cowplot
 #' @examples
 #' plot_fig1()
 plot_fig1 <- function(){
-
   panel_b <- ggplot(ex_growing_epidemic_data, aes(x=time, y=value, color=NA, fill= variable)) +
     geom_bar(stat="identity", width=1.05)+
     geom_segment(data=growing_arrows[growing_arrows$type=="import",], aes(x=time, xend=time, y=yval+1.5, yend=yval),
@@ -34,9 +33,81 @@ plot_fig1 <- function(){
     labs(x = "Time (days)", y="Prevalence", fill="")
 
 
-
   ggdraw() +
     draw_plot(panel_a, x =  0,y =  0.5,width =  1,height =  0.5) +
     draw_plot(panel_b, x =  0,y =  0,width =  1, height = .5) +
     draw_plot_label(c("A", "B"), c(0, 0), c(1, 0.5), size = 15)
+}
+
+
+#' Plot Fig 2 from Zika risk assessment
+#'
+#' @param panels Optional character argument ("a", "b", "c") specifying individual desired panels to return. Accepts uppercase letters as well.
+#' @return A ggplot object for specified plot
+#' @export
+#' @import cowplot
+#' @examples
+#' plot_fig2()
+#' plot_fig2(panels="b")
+plot_fig2 <- function(panels=NULL){
+
+  panel_a <- ggplot(ex_outbreak_sims, aes(time, Cum_Detections, group=interaction(.id, risk_level), color=risk_level)) +
+    geom_line(alpha=0.15,size=1) +
+    scale_color_brewer(palette="Set1", direction = 1)+
+    coord_cartesian(xlim=c(0,150), ylim=c(0,50), expand=FALSE) +
+    guides(color=guide_legend(override.aes=list(alpha=1))) +
+    theme(legend.position=c(0.3,0.8))+
+    labs(x = "Time (days)",
+         y = "Reported Autochthonous Cases",
+         color = expression("R"[0]))
+
+  if(!is.null(panels) & identical(tolower(panels), "a")){
+    return(panel_a)
+  }
+
+  panel_b <- ggplot(prev_by_reported_data, aes(detected, median, color=risk_level, fill=risk_level, linetype=as.factor(disc_prob), group = interaction(risk_level, disc_prob))) +
+    geom_line(size=1)+
+    #geom_hline(yintercept=20)+
+    geom_ribbon(aes(ymax=max, ymin=min), alpha=0.1, color=NA)+
+    scale_y_log10(expand=c(0,0),limits=c(1,50), breaks = c(5,10,25,50))+
+    coord_cartesian(xlim = c(0,15))+
+    scale_x_continuous(expand=c(0.01,0.01))+
+    theme(legend.position = c(0.3,0.79),
+          #legend.direction = "horizontal",
+          legend.box="horizontal")+
+    scale_color_brewer(palette="Set1", direction = 1)+
+    scale_fill_brewer(palette="Set1", direction=1)+
+    guides(linetype=guide_legend(title.hjust = 0, override.aes=list("fill"=NA), title="Reporting Rate"),
+           color=FALSE,fill=FALSE)+
+    labs(x = "Reported Autochthonous Cases",
+         y = "Autochthonous Cases (log scale)",
+         color = expression("R"[0]),
+         fill = expression("R"[0]))
+
+  if(!is.null(panels) & identical(tolower(panels), "b")){
+    return(panel_b)
+  }
+
+  panel_c <- ggplot(epi_prob_by_reported_data, aes(detected, value, linetype=as.factor(disc_prob), color=risk_level)) +
+    geom_line(size=1) +
+    coord_cartesian(xlim=c(0,15), ylim=c(0,1), expand=FALSE)+
+    geom_vline(xintercept=2, size=0.5, linetype=2)+
+    scale_color_brewer(palette="Set1", direction = 1) +
+    background_grid(major = "xy", minor = "none")+
+    theme(legend.position="none",
+          legend.box.just="left")+
+    labs(x = "Reported Autochthonous Cases",
+         y = "Epidemic Probability",
+         color = "Trigger Type",
+         linetype= "County Risk")
+
+  if(!is.null(panels) & identical(tolower(panels), "c")){
+    return(panel_c)
+  }
+
+  ggdraw() + draw_plot(panel_a, x = 0, y=0, width=.33, height=1)+
+    draw_plot(plot = panel_b, x = 0.33, y=0.0, width=0.33, height=1)+
+    draw_plot(plot = panel_c, x = 0.66, y=0.0, width=0.33, height=1)+
+    draw_plot_label(c("A", "B", "C"), c(0, 0.33, 0.66), c(1, 1, 1), size = 20)
+
 }
